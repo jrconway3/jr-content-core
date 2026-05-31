56cbd23544c323f25e48ba942660d2d739bcdf2b
JaidynReiman <jaidyn@jaidynreiman.net>
2026-05-30 21:48:03 -0500

    * Moved REST Content to New Plugin

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -9,5 +9,4 @@ function jr_content_core_bootstrap() {
 	add_action( 'init', 'jr_content_core_register_taxonomies', 6 );
 	add_action( 'init', 'jr_content_core_register_meta', 7 );
 	add_action( 'init', 'jr_content_core_maybe_upgrade', 20 );
-	add_action( 'rest_api_init', 'jr_content_core_register_rest_routes' );
 }
